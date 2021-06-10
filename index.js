f642dfe7cf88867283da34e8825f0b1d3a925c54
trilochan <trilochanreddy100@gmail.com>
2021-06-10 12:55:14 +0000

    solution

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const getDateAfterXDays = (days) => {
   const newDate = addDays(new Date(22, 7, 2020), days);
   return "${newDate.getDate()}-
   ${newDate.getMonth()+1}-
-    ${newDate.getYear()}";
+    ${newDate.getFullYear()}";
 };
 
 module.exports = getDateAfterXDays;
